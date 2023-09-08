bc5b238b32649d8c93765b3f4a80146b543c69a7
Admin <Admin@192.168.1.6>
2023-09-08 21:45:47 +0300

    Good

---
+++ test1/Snake2/src/Snake.h
@@ -0,0 +1,53 @@
+
+#ifndef SNAKE_H_
+#define SNAKE_H_
+
+#include "InputScanner.h"
+
+class Field;
+class CellContent;
+class SnakeBody;
+
+class SnakeHead : public CellContent{
+private:
+	SnakeBody *ptrNextBodyPart;
+	SnakeBody *ptrTail;
+	int x, y;
+	int tailLastPositionX, tailLastPositionY;
+	void draw();
+	void switchDir(int &x, int &y, direction &dir);
+	bool isNeck(Field *fill, int neckX, int neckY);
+	direction dir;
+public:
+	SnakeHead(int x, int y, Field *fill);
+	int tempX;
+	int tempY;
+	virtual void move(Field *fill);
+	SnakeBody* getPtrNextBodyPart();
+	int getTailLastPositionX();
+	int getTailLastPositionY();
+	void setTailLastPositionXY(int x, int y);
+	void setTail(SnakeBody *newTail);
+	void setPtrNextBodyPart(SnakeBody *ptrNextBodyPart);
+	SnakeBody* getPtrTail();
+protected:
+
+};
+
+class SnakeBody : public CellContent{
+private:
+	virtual void draw();
+	int x, y;
+public:
+	virtual void collision(CellContent *faced, Field *fill);
+	SnakeBody(SnakeHead *ptrSnakeHead);
+	void SnakeBodyMove(int x, int y, Field *fill, SnakeHead *snakeHead);
+	int getX();
+	int getY();
+	SnakeBody *ptrNextBodyPart;
+};
+
+
+
+
+#endif
