477526b32bbecd49437ec9a51aa6f490b483a340
Admin <Admin@192.168.1.5>
2023-09-09 19:02:13 +0300

    little test

---
+++ Snake2/src/Util.cpp
@@ -0,0 +1,11 @@
+#include <random>
+#include "Util.h"
+
+using namespace std;
+
+int getRandNumInt(){
+	static random_device rd;
+	static mt19937 mersenne(rd());
+	uniform_int_distribution<int> distribution(0, 9);
+	return distribution(mersenne);
+}
